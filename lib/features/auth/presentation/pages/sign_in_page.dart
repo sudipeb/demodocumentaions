55316b27e8bb55a804db56891e5ce2e8a2aef207
sudipeb <sudip.koirala@ebpearls.com>
2025-12-11 15:31:48 +0545

    added the google auth

--- lib/features/auth/presentation/pages/sign_in_page.dart
+++ lib/features/auth/presentation/pages/sign_in_page.dart
@@ -1,15 +1,61 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({super.key});
+
+  void _handleAuthenticationEvent(GoogleSignInAuthenticationEvent event) {
+    // Handle successful authentication
+    debugPrint('Authentication event: $event');
+  }
+
+  void _handleAuthenticationError(Object error) {
+    // Handle authentication errors
+    debugPrint('Authentication error: $error');
+  }
+
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
+    final GoogleSignIn signIn = GoogleSignIn.instance;
+    unawaited(
+      signIn
+          .initialize(
+            clientId:
+                "1089380465346-72ucmurv2iigj0sd1asu1hrefvofji67.apps.googleusercontent.com",
+            serverClientId:
+                "1089380465346-72ucmurv2iigj0sd1asu1hrefvofji67.apps.googleusercontent.com",
+          )
+          .then((_) {
+            signIn.authenticationEvents
+                .listen(_handleAuthenticationEvent)
+                .onError(_handleAuthenticationError);
+
+            /// This example always uses the stream-based approach to determining
+            /// which UI state to show, rather than using the future returned here,
+            /// if any, to conditionally skip directly to the signed-in state.
+            signIn.attemptLightweightAuthentication();
+          }),
+    );
     return Scaffold(
       appBar: AppBar(title: Text("Google Sign In")),
       body: SizedBox(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [Center(child: Text("Verification of Sign In"))],
+          children: [
+            if (GoogleSignIn.instance.supportsAuthenticate())
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    await GoogleSignIn.instance.authenticate();
+                  } catch (e) {
+                    debugPrint('Caught$e');
+                  }
+                },
+                child: const Text('SIGN IN'),
+              ),
+          ],
         ),
       ),
     );
